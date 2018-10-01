0a6b972f6ba721cc72ea0272152ec0d1252e6036
HinaYashvi <hina.parmar@staroneweb.co.in>
2018-10-01 18:02:12 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -301,7 +301,14 @@ function checklogin(){
 //*************************************************************************************************** //
 $$(document).on('page:init', '.page[data-name="index"]', function (e) { 
   checkConnection();  
-  app.panel.disableSwipe('left');
+ // app.panel.disableSwipe('left');
+  var page = e.detail;
+  if(page.name=='index'){
+    alert("page.name="+page.name);
+    app.panel.disableSwipe('left');
+  }
+  //app.panel.close();
+  //app.panel.destroy();
   var url = base_url+"app_controller/getAppCity";
   $.ajax({
     'url': url, 
