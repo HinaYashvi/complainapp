b2da9b104bf28069388a77d5f800b28b7942ce3a
HinaYashvi <hina.parmar@staroneweb.co.in>
2018-10-03 15:48:31 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1327,8 +1327,9 @@ function downloaddoc(fullpath,folder_path){
      //alert("in Android");
     var assetURL = fullpath;
     //var store = cordova.file.externalRootDirectory+"Download/"; // output in android: file:///storage/emulated/0/
-    var store = cordova.file.dataDirectory; // or //var store = "cdvfile://localhost/persistent/";
+    //var store = cordova.file.dataDirectory; // or //var store = "cdvfile://localhost/persistent/";
     //var store = cordova.file.documentsDirectory;
+    var store = "file:///storage/emulated/0/";
     
     alert(store);
    // alert(assetURL);
