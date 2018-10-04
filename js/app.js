991383ae95abbce5f39bed57677a9d27b7063d24
HinaYashvi <hina.parmar@staroneweb.co.in>
2018-10-04 10:50:16 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1337,7 +1337,8 @@ function downloaddoc(fullpath,folder_path){
    // var store = cordova.file.externalApplicationStorageDirectory; // working : inside com.phonegap.yourcollector //
    // var store = cordova.file.externalDataDirectory;   // working : inside com.phonegap.yourcollector/files //
     
-    var store = cordova.file.externalRootDirectory+"Download/";
+   // var store = cordova.file.externalRootDirectory+"Download/"; // not working  : path - file://storage/emulated/0/Download/    //
+    var store = cordova.file.tempDirectory;
     
    /* var store = cordova.file.applicationStorageDirectory;
     var store2 = cordova.file.dataDirectory;
